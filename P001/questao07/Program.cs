b100d94598c8149391a6d9083b5bea676e0c9517
Luiz Felipe <101591932+lufecrx@users.noreply.github.com>
2023-11-20 06:02:57 -0300

    Delete P001 directory

--- P001/questao07/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿bool condicao1 = true;
-bool condicao2 = false;
-
-if (condicao1 && condicao2)
-    Console.WriteLine("As duas condições são verdadeiras");
-else
-    Console.WriteLine("Uma ou mais condições são falsas");
-
-Console.ReadKey();
