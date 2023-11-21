f4ad7827f9f9b1114c24dd86c2e725eaa47c5ed9
lufecrx <luizfelipecrx@gmail.com>
2023-11-21 14:44:37 -0300

    Adicionar exercício da semana 3

---
+++ Exercicios/LINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿#region Tuple Exercises 01
+(string, int) GetAge (string name, DateTime born)
+{ 
+    int age = DateTime.Now.Year - born.Year;
+    if (DateTime.Now.DayOfYear < born.DayOfYear)
+        age--;
+    return (name, age);
+}
+
+var getData = GetAge("Luiz", new DateTime(2003, 7, 6));
+Console.WriteLine($"{getData.Item1} tem {getData.Item2} anos.");
+#endregion
